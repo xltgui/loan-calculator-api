a25a8535774adcc439dd86335bb1fd3cff6de8bc
xltgui <borba.liver@gmail.com>
2025-08-08 09:26:31 -0300

    feat: habilitando cross origin

--- src/main/java/com/github/xltgui/loancaculatorchallenge/web/LoanCalculatorController.java
+++ src/main/java/com/github/xltgui/loancaculatorchallenge/web/LoanCalculatorController.java
@@ -6,16 +6,14 @@ import com.github.xltgui.loancaculatorchallenge.model.LoanCalculationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/loan-calculate")
+@CrossOrigin(origins = "http://localhost:4200")
 public class LoanCalculatorController {
     private final LoanCalculationService service;
     private final LoanCalculatorMapper mapper;
