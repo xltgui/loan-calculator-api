c489489cd9ec556bcec0136ee070306a2926c626
xltgui <borba.liver@gmail.com>
2025-08-09 15:12:40 -0300

    fix: fixing unused imports

--- src/main/java/com/github/xltgui/loancaculatorchallenge/model/Loan.java
+++ src/main/java/com/github/xltgui/loancaculatorchallenge/model/Loan.java
@@ -1,7 +1,6 @@
 package com.github.xltgui.loancaculatorchallenge.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
